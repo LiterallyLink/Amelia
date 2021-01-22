da7870f37eb661fd08c8ecc2129106dc283eeac5
LiterallyLink <67402756+LiterallyLink@users.noreply.github.com>
2021-01-22 14:32:57 -0800

    Created blackjack command
    Clean up later, and move functions

--- src/Commands/Fun/blackjack.js
+++ src/Commands/Fun/blackjack.js
@@ -1,21 +1,48 @@
+/* eslint-disable consistent-return */
 const Command = require('../../Structures/Command');
 const { MessageEmbed } = require('discord.js');
+const { stripIndents } = require('common-tags');
+const emoji = require('../../../assets/JSONS/blackjackEmojis.json');
 
+const input = ['hit', 'stay'];
 
 module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
 			category: 'Fun',
-			guildOnly: true
+			usage: '<credits>',
+			guildOnly: true,
+			args: true
 		});
 	}
 
-	async run(message) {
+	async run(message, [bet]) {
+		const balance = await this.client.economy.getCredits(message.guild.id, message.author.id);
+
+		if (!this.client.utils.isWhole(bet) || bet <= 0) {
+			return message.channel.send('invalid credits');
+		} else if (bet > balance) {
+			return message.channel.send(new MessageEmbed().setDescription(`You don't have enough credits.\nYou have ${balance} credits`).setColor('RED'));
+		}
+
+		const current = this.client.games.get(message.channel.id);
+
+		if (current) {
+			return message.reply(new MessageEmbed().setColor('RED').setDescription(`Please wait until the current game of \`${current.name}\` is finished.`));
+		}
+
+		this.client.games.set(message.channel.id, {
+			name: this.name
+		});
+
 		const suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
 		const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 		let players = [];
+		const playerHand = [];
+		const dealersHand = [];
 		let deck = [];
+		let win = false;
 
 		function createDeck() {
 			deck = [];
@@ -24,7 +51,11 @@ module.exports = class extends Command {
 					let weight = parseInt(values[i]);
 					if (values[i] === 'J' || values[i] === 'Q' || values[i] === 'K') { weight = 10; }
 					if (values[i] === 'A') { weight = 11; }
-					const card = { Value: values[i], Suit: suits[j], Weight: weight };
+					const card = {
+						Value: values[i],
+						Suit: suits[j],
+						Weight: weight
+					};
 					deck.push(card);
 				}
 			}
@@ -46,14 +77,19 @@ module.exports = class extends Command {
 			players = [];
 			for (let i = 1; i <= 2; i++) {
 				const hand = [];
-				const player = { Name: `Player ${i}`, ID: i, Points: 0, Hand: hand };
+				const player = {
+					Name: `Player ${i}`,
+					ID: i,
+					Points: 0,
+					Hand: hand
+				};
 				players.push(player);
 			}
 		}
 
 		function getPoints(player) {
-			var points = 0;
-			for (var i = 0; i < players[player].Hand.length; i++) {
+			let points = 0;
+			for (let i = 0; i < players[player].Hand.length; i++) {
 				points += players[player].Hand[i].Weight;
 			}
 			players[player].Points = points;
@@ -61,7 +97,7 @@ module.exports = class extends Command {
 		}
 
 		function updatePoints() {
-			for (var i = 0; i < players.length; i++) {
+			for (let i = 0; i < players.length; i++) {
 				getPoints(i);
 			}
 		}
@@ -76,18 +112,96 @@ module.exports = class extends Command {
 			}
 		}
 
+		function getHands() {
+			for (let j = 0; j < 2; j++) {
+				playerHand.push(`${emoji[players[0].Hand[j].Value + players[0].Hand[j].Suit]}`);
+				dealersHand.push(`${emoji[players[1].Hand[j].Value + players[1].Hand[j].Suit]}`);
+			}
+		}
+
+		function hitMe(num, hand) {
+			const card = deck.pop();
+			players[num].Hand.push(card);
+			hand.push(`${emoji[card.Value + card.Suit]}`);
+			updatePoints();
+		}
+
 		createDeck();
 		shuffle();
 		createPlayers();
 		dealHands();
+		getHands();
 
 		const embed = new MessageEmbed()
-			.setDescription(`${players[0].Hand[0].Value}${players[0].Hand[0].Suit} ${players[0].Hand[1].Value}${players[0].Hand[1].Suit} -- ${players[0].Points}\n
-            ${players[1].Hand[0].Value}${players[0].Hand[0].Suit} ${players[1].Hand[1].Value}${players[0].Hand[1].Suit} -- ${players[1].Points}`);
-		const msg = await message.channel.send(embed);
-		msg.react('🛑');
-		msg.react('✅');
-		msg.react('🟡');
+			.setAuthor('Blackjack', message.author.displayAvatarURL())
+			.setDescription(stripIndents`**Your Hand: ${players[0].Points}\n${playerHand.join('')}\nThe dealer shows:**\n${dealersHand[0]}`)
+			.setFooter('Hit or stay?')
+			.setColor('fce3b7');
+		message.channel.send(embed);
+
+		try {
+			while (!win) {
+				const filter = res => res.author.id === message.author.id && input.includes(res.content.toLowerCase());
+				const msgs = await message.channel.awaitMessages(filter, {
+					max: 1,
+					time: 60000
+				});
+
+				if (!msgs.size) {
+					this.client.games.delete(message.channel.id);
+					return message.channel.send('a');
+				}
+
+				const choice = msgs.first().content.toLowerCase();
+
+				if (choice === input[0]) {
+					hitMe(0, playerHand);
+
+					if (players[0].Points > 21) {
+						await this.client.economy.addCredits(message.guild.id, message.author.id, bet * -1);
+						this.client.games.delete(message.channel.id);
+						message.channel.send(embed
+							.setDescription(stripIndents`**Your Hand: ${players[0].Points}\n${playerHand.join('')}\nDealer's cards: ${players[1].Points}**\n${dealersHand.join('')}`)
+							.setFooter(`🚫 Bust!`));
+						win = true;
+						break;
+					}
+					message.channel.send(embed.setDescription(stripIndents`**Your Hand: ${players[0].Points}\n${playerHand.join('')}\nThe dealer shows:**\n${dealersHand[0]}`));
+				} else if (choice === input[1]) {
+					while (players[0].Points > players[1].Points && players[1].Points <= 21) {
+						hitMe(1, dealersHand);
+					}
+
+					this.client.games.delete(message.channel.id);
+					if (players[0].Points > players[1].Points || players[1].Points > 21) {
+						await this.client.economy.addCredits(message.guild.id, message.author.id, bet);
+						message.channel.send(embed
+							.setDescription(stripIndents`**Your Hand: ${players[0].Points}\n${playerHand.join('')}\nDealer's cards: ${players[1].Points}**\n${dealersHand.join('')}`)
+							.setFooter(`💸 You won ${bet * 2} credits!`));
+						win = true;
+						break;
+					} else if (players[0].Points < players[1].Points) {
+						await this.client.economy.addCredits(message.guild.id, message.author.id, bet * -1);
+						this.client.games.delete(message.channel.id);
+						message.channel.send(embed
+							.setDescription(stripIndents`**Your Hand: ${players[0].Points}\n${playerHand.join('')}\nDealer's cards: ${players[1].Points}**\n${dealersHand.join('')}`)
+							.setFooter(`The dealer won!`));
+						win = true;
+						break;
+					} else {
+						this.client.games.delete(message.channel.id);
+						message.channel.send(embed
+							.setDescription(stripIndents`**Your Hand: ${players[0].Points}\n${playerHand.join('')}\nDealer's cards: ${players[1].Points}**\n${dealersHand.join('')}`)
+							.setFooter(`Returned ${bet} credits`));
+						win = true;
+						break;
+					}
+				}
+			}
+		} catch (err) {
+			this.client.games.delete(message.channel.id);
+			throw err;
+		}
 	}
 
 };
